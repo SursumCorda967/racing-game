9ec38ae9e67c267bfc17af162a14f2905ca15bce
SursumCorda967 <142233040+SursumCorda967@users.noreply.github.com>
2023-08-14 12:04:39 -0300

    Update RacingGame.py

--- RacingGame.py
+++ RacingGame.py
@@ -2,98 +2,76 @@ import random
 import os
 import time
 
-def get_user_name():
-    return input("Enter your name: ")
+# ... (Previous code remains the same)
 
-def display_track(player_pos, opponent_pos, race_distance, player_name):
-    track = f'Player: {player_name}\n'
-    track += '-' * player_pos + 'P' + '-' * (race_distance - player_pos)
-    track += '\n' + '-' * opponent_pos + 'O' + '-' * (race_distance - opponent_pos)
-    return track
+def show_power_up_effect(power_up):
+    if power_up == "Speed boost":
+        return "Your speed has increased!"
+    elif power_up == "Invulnerability":
+        return "You are invulnerable for a short time!"
+    elif power_up == "Oil slick":
+        return "You dropped an oil slick on the track!"
+    else:
+        return "Unknown power-up effect"
 
-def get_user_input():
-    return input("Press 'Enter' to accelerate or 'q' to quit: ").strip().lower()
+def apply_power_up(player_position, opponent_position, power_up):
+    if power_up == "Speed boost":
+        player_position += random.randint(2, 5)
+    elif power_up == "Invulnerability":
+        opponent_position -= random.randint(1, 3)
+    elif power_up == "Oil slick":
+        opponent_position -= random.randint(2, 4)
+    return player_position, opponent_position
 
-def countdown():
-    for i in range(3, 0, -1):
-        print(f"Race starting in {i}...")
-        time.sleep(1)
+def display_stats(player_position, opponent_position, race_distance):
+    player_progress = (player_position / race_distance) * 100
+    opponent_progress = (opponent_position / race_distance) * 100
+
+    print("\nRace Progress:")
+    print(f"Player: {'=' * int(player_progress)} ({player_position}/{race_distance})")
+    print(f"Opponent: {'=' * int(opponent_progress)} ({opponent_position}/{race_distance})")
 
 def main():
-    race_distance = 50
-    player_position = 0
-    opponent_position = 0
-    max_acceleration = 5
-    opponent_speed_range = (1, 3)
-
-    print("Welcome to the Racing Game!")
-
-    difficulty = input("Select difficulty (easy, medium, hard): ").strip().lower()
-    if difficulty == "easy":
-        max_acceleration = 3
-        opponent_speed_range = (1, 2)
-    elif difficulty == "hard":
-        max_acceleration = 7
-        opponent_speed_range = (2, 4)
-
-    player_name = get_user_name()
-    os.system('clear')
-
-    countdown()
-
-    tracks = [
-        "Track 1: A winding road through the mountains.",
-        "Track 2: A fast and furious race through the city.",
-        "Track 3: A challenging off-road course through the desert."
-    ]
-
-    cars = [
-        "Car 1: A powerful sports car.",
-        "Car 2: A sleek and stylish sports car.",
-        "Car 3: A rugged and durable off-road vehicle."
-    ]
-
-    power_ups = [
-        "Speed boost",
-        "Invulnerability",
-        "Oil slick"
-    ]
+    # ... (Previous code remains the same)
 
     while player_position < race_distance and opponent_position < race_distance:
+        os.system('clear')
+        display_stats(player_position, opponent_position, race_distance)
         print(display_track(player_position, opponent_position, race_distance, player_name))
         user_input = get_user_input()
-        
+
         if user_input == 'q':
             print("Quitting the game...")
             break
-        
-        # Choose a random track
+
+        # Choose a random track, car, and power-up
         track = random.choice(tracks)
-        print(f"You're racing on {track}!")
-
-        # Choose a random car
         car = random.choice(cars)
-        print(f"You're driving a {car}!")
-
-        # Choose a random power-up
         power_up = random.choice(power_ups)
-        print(f"You found a {power_up}!")
 
-        # Add some randomness to the player's acceleration
+        print(f"You're racing on {track}!")
+        print(f"You're driving a {car}!")
+        print(f"You found a {power_up}!")
+        print(show_power_up_effect(power_up))
+
         player_acceleration = random.randint(1, max_acceleration)
-        # Make the opponent's speed increase with difficulty
         opponent_speed = random.randint(*opponent_speed_range) + difficulty
-        
+
         player_position += player_acceleration
         opponent_position += opponent_speed
-        
-        # Add a delay to make the game more realistic
-        time.sleep(0.5)
+
+        if random.random() < 0.2:
+            player_position, opponent_position = apply_power_up(player_position, opponent_position, power_up)
+
+        time.sleep(1)  # Add a delay to make the game more realistic
+
+    os.system('clear')
+    display_stats(player_position, opponent_position, race_distance)
 
     if player_position >= race_distance:
-        print("Congratulations! You won!")
+        print("\nCongratulations! You won the race!")
     else:
-        print("Sorry, you lost. Better luck next time!")
+        print("\nSorry, you lost. Better luck next time!")
 
 if __name__ == "__main__":
     main()
